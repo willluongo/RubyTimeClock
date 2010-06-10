ad0577e96afedd9a9325eb853697ee8fea2c07c4
Will Luongo <me@willluongo.com>
2010-06-10 13:17:25 -0400

    new file:   main.rb

---
+++ main.rb
@@ -0,0 +1,140 @@
+# TimeClock V2
+# Basic timeclock functionality
+
+
+require "sinatra"
+require 'rubygems'
+require 'dm-core'
+require 'digest/sha1'
+
+DataMapper.setup(:default, 'mysql://rubytest:rubytestpass@local.derbserv.org/rubytest')
+
+class Punch
+  include DataMapper::Resource
+  property :id, Serial
+  property :username, String
+  property :punchtime, DateTime
+  property :punchstate, String
+end
+
+class User
+  include DataMapper::Resource
+  property :id, Serial
+  property :username, String
+  property :password, String
+  property :cookie, String
+  property :admin, String
+end
+
+DataMapper.auto_upgrade!
+
+get '/' do
+  username = cookiecheck(request.cookies["MainSiteKey"])
+  if !username
+    redirect '/login'
+  end
+  punchstate=getstate(username)
+
+  return <<-EOL
+  <title>Basic Timeclockiness</title>
+  <body>
+  <form method=post action="submit" />
+  <input type="submit" value="Punch #{username} #{punchstate}" />
+  </form>
+  </body>
+  EOL
+end
+
+post '/submit' do
+
+  username = cookiecheck(request.cookies["MainSiteKey"])
+  if !username
+    redirect '/login'
+  end
+  punchstate = getstate(username)
+
+  Punch.create(
+    :username =>  username,
+    :punchtime  =>  Time.now,
+    :punchstate =>  punchstate
+  )
+  return "Punch #{punchstate} for #{username} successful!"
+end
+
+get '/login' do
+  return <<-EOL
+  <title>Basic Timeclockiness</title>
+  <body>You must log in:<br/>
+  <form method=post action="loginsubmit" />
+Username:<br/>
+  <input type="text" name="username" /><br/>
+Password:<br/>
+  <input type="text" name="password" />
+  <input type="submit" value="Log in" />
+  </form>
+  </body>
+  EOL
+end
+
+post '/loginsubmit' do
+  username=params[:username]
+  password=params[:password]
+  if passcheck(username,password)
+    redirect '/'
+  else
+    redirect '/login'
+  end
+  
+end
+
+
+get '/admin' do
+
+end
+
+get '/report' do
+
+end
+
+
+# Checks for a cookie, returns a username
+def cookiecheck(sessioncookie)
+
+  whoitbe = User.first(:cookie => sessioncookie)
+  if whoitbe
+    loggedusername = whoitbe.username
+    return loggedusername
+  end
+
+end
+
+def getstate(username)
+  punchstate=''
+    lastpunch = Punch.all(:username => username)
+    if !lastpunch.last
+      punchstate='IN'
+    else
+      if lastpunch.last.punchstate == 'IN'
+        punchstate = 'OUT'
+        else
+        punchstate = 'IN'
+      end
+    end
+
+  return punchstate
+end
+
+def passcheck(username,attempt)
+  passinfo=User.first(:username => username)
+  if !passinfo
+    redirect '/login'
+  end
+  if Digest::SHA1.hexdigest(attempt) == passinfo.password
+    timestamp=Time.now.strftime("%Y%m%d%H%M%S")
+    uniquestamp = username << timestamp
+    digest = Digest::MD5.hexdigest(uniquestamp)
+    response.set_cookie("MainSiteKey", {:value => digest, :expiration => Time.now + 600})
+    passinfo.update(:cookie => digest)
+    return "success"
+  end
+end
